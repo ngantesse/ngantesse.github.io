9bde9d94250eb226d7dcd791821a60b84284e16b
ngantesse <46992589+ngantesse@users.noreply.github.com>
2019-01-24 18:56:26 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,72 +47,75 @@
     }
     ];
 
-  function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
+  function buildQuiz(){
+  // we'll need a place to store the HTML output
+  const output = [];
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
+  // for each question...
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
       // we'll want to store the list of answer choices
       const answers = [];
 
       // and for each available answer...
-      for (letter in currentQuestion.answers) {
+      for(letter in currentQuestion.answers){
+
         // ...add an HTML radio button
         answers.push(
           `<label>
-             <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-           </label>`
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
         );
       }
 
       // add this question and its answers to the output
       output.push(
-        `<div class="slide">
-           <div class="question"> ${currentQuestion.question} </div>
-           <div class="answers"> ${answers.join("")} </div>
-         </div>`
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
       );
-    });
+    }
+  );
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+function showResults(){
 
-  function showResults() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
+  // gather answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll('.answers');
 
-    // keep track of user's answers
-    let numCorrect = 0;
+  // keep track of user's answers
+  let numCorrect = 0;
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  // for each question...
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
 
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = 'input[name=question'+questionNumber+']:checked';
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-        // color the answers green
-        answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
+    // if answer is correct
+    if(userAnswer===currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
+      // color the answers green
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
 
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
+}
   function showSlide(n) {
     slides[currentSlide].classList.remove("active-slide");
     slides[n].classList.add("active-slide");
